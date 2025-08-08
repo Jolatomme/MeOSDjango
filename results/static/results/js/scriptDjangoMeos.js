702ca67d77dfb7535fc6b739beee721e3cf54cd6
statinfo <laurent.merat@gmail.com>
2025-08-08 23:06:03 +0200

    toggle full vs simple results
    permet de passer de la présentation simple à détaillée avec un bouton switch, et conserve l'option en changeant de catégorie

--- results/static/results/js/scriptDjangoMeos.js
+++ results/static/results/js/scriptDjangoMeos.js
@@ -1,5 +1,5 @@
-//backtotop button
-let mybutton = document.getElementById("btn-back-to-top");
+//backToTop button
+let backToTop = document.getElementById("btn-back-to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
@@ -8,15 +8,48 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    backToTop.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    backToTop.style.display = "none";
   }
 }
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
 
-function backToTop() {
+// When the user clicks on the button, scroll to the top of the document
+backToTop.addEventListener("click", scrollBackToTop);
+
+function scrollBackToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+
+// switch between full/simple results
+let switchFullDetail = document.getElementById("switchFullDetail");
+
+function toggleSwitchFullDetail () {
+	let addr = new URL(window.location);
+	if (addr.pathname.includes("/cat")) {
+		// changement de page pour toggle entre full/simple
+		if (switchFullDetail.checked == true) {
+			pathname = addr.pathname += "detail/";
+		} else {
+			pathname = addr.pathname.replace("detail/", '');
+		}
+		window.location.href = addr.origin+pathname+addr.search;
+	} else {
+		// changement seulement des urls des boutons de catégories
+		let listButton = document.querySelectorAll("#linkToCls a");
+		listButton.forEach( item => {
+			addr = new URL(item.href);
+			if (switchFullDetail.checked == true) {
+				pathname = addr.pathname += "detail/";
+			} else {
+				pathname = addr.pathname.replace('detail/', '');
+			}
+			item.href = pathname + addr.search;
+		})
+	}
+}
+
+switchFullDetail.addEventListener("click", toggleSwitchFullDetail);
