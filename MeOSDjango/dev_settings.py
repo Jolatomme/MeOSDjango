9410886b2313387eb5d03c0d16556346ce8c2875
Jolatomme <severin.trochut@libertysurf.fr>
2026-08-07 18:06:52 +0200

    wrongly pushed

--- MeOSDjango/dev_settings.py
+++ MeOSDjango/dev_settings.py
@@ -1,9 +1,9 @@
-DATABASE_NAME = "jolatomme_meos"
-DATABASE_USER = "jolatomme_sqlreader"
-DATABASE_PASSWORD = "MeOS_SQLReader1"
-DATABASE_HOST = "mysql-jolatomme.alwaysdata.net"
+DATABASE_NAME = "Nom de la base de données"
+DATABASE_USER = "Utilisateur de la base de données"
+DATABASE_PASSWORD = "Mot de passe pour accéder à la base de données"
+DATABASE_HOST = "mysql-XXX.alwaysdata.net"
 DATABASE_PORT = "3306"
 
-MEOS_PASSWORD = "meos_password"
+MEOS_PASSWORD = "Password MeOS"
 
 DJANGO_SECRET_KEY = "La clé générée juste avant"
