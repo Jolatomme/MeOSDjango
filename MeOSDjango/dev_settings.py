becdc511da828ca8ef76bf6ce27731c0e6182612
Jolatomme <severin.trochut@libertysurf.fr>
2026-08-07 15:27:29 +0200

    mise à jour des contributeurs

--- MeOSDjango/dev_settings.py
+++ MeOSDjango/dev_settings.py
@@ -1,9 +1,9 @@
-DATABASE_NAME = 'Nom de la database créée précédemment'
-DATABASE_USER = 'Nom de l'utilisateur qui va accéder à la database'
-DATABASE_PASSWORD = 'Mot de passe de l'utilisateur'
-DATABASE_HOST = 'mysql-XXXXXXXXX.alwaysdata.net'
-DATABASE_PORT = '3306'
+DATABASE_NAME = "Nom de la database créée précédemment"
+DATABASE_USER = "Nom de l'utilisateur qui va accéder à la database"
+DATABASE_PASSWORD = "Mot de passe de l'utilisateur"
+DATABASE_HOST = "mysql-XXXXXXXXX.alwaysdata.net"
+DATABASE_PORT = "3306"
 
-MEOS_PASSWORD = 'le mot de passe de meos pour envoyer les données à la database'
+MEOS_PASSWORD = "le mot de passe de meos pour envoyer les données à la database"
 
-DJANGO_SECRET_KEY = 'La clé générée juste avant'
+DJANGO_SECRET_KEY = "La clé générée juste avant"
