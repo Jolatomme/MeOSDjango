a0b2090f7985f0233df4737e30938d7fc5b1e34e
trochut <severin.trochut@libertysurf.fr>
2026-08-24 20:10:22 +0200

    Exclude dev_settings.py from git status and add generic dev_settings.py

---
+++ MeOSDjango/dev_settings.py
@@ -0,0 +1,9 @@
+DATABASE_NAME = "Nom de la base de données"
+DATABASE_USER = "Utilisateur de la base de données"
+DATABASE_PASSWORD = "Mot de passe pour accéder à la base de données"
+DATABASE_HOST = "mysql-XXX.alwaysdata.net"
+DATABASE_PORT = "3306"
+
+MEOS_PASSWORD = "Password MeOS"
+
+DJANGO_SECRET_KEY = "La clé générée juste avant"
